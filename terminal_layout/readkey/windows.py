89b99fbee992a688c4c8f0b8ccebab360139b055
gojuukaze <ikaze_email@163.com>
2020-06-06 15:01:06 +0800

    windows readkey bug

--- terminal_layout/readkey/windows.py
+++ terminal_layout/readkey/windows.py
@@ -3,19 +3,26 @@
 # http://code.activestate.com/recipes/134892/#c9
 # Thanks to Stephen Chappell
 import msvcrt
-import sys
 
 from terminal_layout.readkey.key import Key
+from terminal_layout.logger import logger
 
-mapping = {
-    13: Key.ENTER,
-    27: Key.ESC,
+xlate_dict = {
+    13: Key.ENTER.code,
+    27: Key.ESC.code,
+    15104: Key.F1.code,
+    15360: Key.F2.code,
+    15616: Key.F3.code,
+    15872: Key.F4.code,
+    16128: Key.F5.code,
+    16384: Key.F6.code,
+    16640: Key.F7.code,
+    16896: Key.F8.code,
 
-    18656: Key.UP,
-    20704: Key.DOWN,
-    19424: Key.LEFT,
-    19936: Key.RIGHT,
-    b'\x08': Key.BACKSPACE
+    18656: Key.UP.code,
+    20704: Key.DOWN.code,
+    19424: Key.LEFT.code,
+    19936: Key.RIGHT.code,
 }
 
 
@@ -27,9 +34,6 @@ def readkey():
             if a == 0 or a == 224:
                 b = ord(msvcrt.getch())
                 x = a + (b * 256)
-                try:
-                    return mapping[x]
-                except KeyError:
-                    return None
+                return xlate_dict.get(x, None)
             else:
                 return ch.decode()
