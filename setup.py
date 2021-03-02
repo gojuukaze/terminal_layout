bd827c538c1c383928e37bbb789319a333c4d989
gojuukaze <ikaze_email@163.com>
2021-03-02 16:56:41 +0800

    升级colorama版本

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="terminal_layout",
-    version="2.0.3",
+    version="2.0.4",
     description="The project help you to quickly build layouts in terminal (命令行ui布局工具)",
     long_description=open("README.rst", encoding='utf-8').read(),
 
@@ -18,10 +18,10 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     include_package_data=True,
     platforms="OSX, Linux",
-    install_requires=['colorama==0.4.1',
+    install_requires=['colorama==0.4.4',
                       'colored==1.3.93',
                       'backports.shutil_get_terminal_size'],
-    tests_require=['colorama==0.4.1',
+    tests_require=['colorama==0.4.4',
                    'colored==1.3.93'],
 
     classifiers=[
