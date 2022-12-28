b4f4be98708262e1670136899d17e411fd6a352c
gojuukaze <ikaze_email@163.com>
2022-12-28 11:31:17 +0800

    移除兼容py2的代码

--- terminal_layout/helper/helper.py
+++ terminal_layout/helper/helper.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-try:
-    from os import get_terminal_size
-except:
-    from backports.shutil_get_terminal_size import get_terminal_size
-
+from shutil import get_terminal_size
 
 def get_terminal_size2():
     """
     之前写的，暂时不用了
     """
     r = subprocess.check_output("stty size", shell=True)
-    try:
-        size = str(r, encoding="utf8").strip().split(' ')
-    except:
-        # py2
-        size = str(r).strip().split(' ')
+    size = str(r, encoding="utf8").strip().split(' ')
     return int(size[0]), int(size[1])
 
 
